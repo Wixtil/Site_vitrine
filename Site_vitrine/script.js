c6747e93f9a1ffac5dd82235053a198cc8ffc98a
Wixtil <103941632+Wixtil@users.noreply.github.com>
2023-04-05 23:21:07 +0200

    :+1:
    Site portfolio

--- Site_vitrine/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function () {
-  var nav = document.querySelector("header");
-  var sticky = nav.offsetTop;
-  if (window.pageYOffset > sticky) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-};
